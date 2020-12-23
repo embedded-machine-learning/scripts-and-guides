79980d7210a5b461c01e5bda8e1c95f13cbb0538
wendt <alexander.wendt@tuwien.ac.at>
2020-12-23 11:37:23 +0100

    Added visualization script for 2 images with opencv

---
+++ scripts/evaluation/visualize_object_detection_images_opencv.bat
@@ -0,0 +1,5 @@
+python visualize_object_detection_images_opencv.py ^
+--image_path1="samples/images/0.jpg" --annotation_dir1="samples/annotations/xml" ^
+--image_path2="samples/images/30.jpg" --annotation_dir2="samples/annotations/xml" ^
+--output_dir="samples/results" ^
+--line_thickness=2
