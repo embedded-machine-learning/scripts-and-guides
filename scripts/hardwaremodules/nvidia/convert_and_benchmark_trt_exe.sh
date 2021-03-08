b2d76f4c0059798c47620623a11d3b71a3bc0b22
themozel <amidmozelli@gmail.com>
2021-03-08 12:46:19 +0100

    bash file edit

--- scripts/hardwaremodules/nvidia/convert_and_benchmark_trt_exe.sh
+++ scripts/hardwaremodules/nvidia/convert_and_benchmark_trt_exe.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-PRECISION = INT8
+#PRECISION = INT8
 
-python3 convert_and_benchmark_trt.py \
+#python3 convert_and_benchmark_trt.py \
 --tensorflow_model=./tf2oda_ssdmobilenetv2_320x320_coco17_D100_pedestrian_test/saved_model/ \
 --precision='INT8' \
 --image_size='[320,320]' \
@@ -21,3 +21,24 @@ python3 convert_and_benchmark_trt.py \
 #--precision='FP32' \
 #--conversion \
 #--data_dir='./data'
+
+#conversion
+python3 convert_and_benchmark_trt_final.py \
+--conversion True \
+--tensorflow_model tf2oda_ssdmobilenetv2_320_320_coco17_D100_pedestrian_test/saved_model \
+--precision FP16 \
+--image_size [320,320] \
+--data_dir='./data/' \
+#--batch_size=1 
+
+
+#benchmark
+python3 convert_and_benchmark_trt.py \
+--net tf2oda_ssdmobilenetv2_320x320_coco17_D100_pedestrian_test/saved_model \
+--dtype uint8 \
+--precision FP32 \
+--image_size [320,320] \
+--data_dir='./data/' \
+--csv=./results.csv
+
+
