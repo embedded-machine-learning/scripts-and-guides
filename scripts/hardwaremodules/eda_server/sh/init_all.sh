befd64935997560a75b52d18a2181b7be6090881
wendt <alexander.wendt@tuwien.ac.at>
2021-03-11 17:07:56 +0100

    Added eda02 specific scripts

---
+++ scripts/hardwaremodules/eda_server/sh/init_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# put in home
+
+echo "Run Task Spooler and TF2 Object Detection init script"
+
+. ~/init_eda_env.sh
+. ~/init_eda_ts.sh
+cd /srv/cdl-eml/tf2odapi
---
+++ scripts/hardwaremodules/eda_server/sh/init_eda_env.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# put in home
+
+echo "WARNING: Start script with . /[PATH_TO_SCRIPT], not with ./"
+echo "WARNING: Set variable ENVROOT to your root for environment and TF2ODA models."
+
+#echo "Setup task spooler socket for GPU."
+
+#export TS_SOCKET="/srv/ts_socket/GPU.socket"
+#chmod 777 /srv/ts_socket/GPU.socket
+#export TS_TMPDIR=/home/$NAME/logs
+#echo task spooler output directory: /home/$NAME/logs
+
+echo "Activate TF2 Object Detection API Python Environment"
+
+TF2ROOT=`pwd`
+ENVROOT=/srv/cdl-eml/tf2odapi
+
+source $ENVROOT/tf24/bin/activate
+cd $ENVROOT/models/research/
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+echo New python path $PYTHONPATH
+
+cd $TF2ROOT
+
+echo "Activation complete"
