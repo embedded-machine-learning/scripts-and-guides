d338e38d7e2fe74ad276e25e8a0f477f8424f93c
jasrial <juju.westra@gmail.com>
2021-05-07 12:35:09 +0200

    Implemented loop through result data. We extract the bounding box data for each iteration, store it in an array. This array is then converted to a dataframe which is then converted to a csv file.

--- scripts/hardwaremodules/openvino/result_parser/test_write_results.py
+++ scripts/hardwaremodules/openvino/result_parser/test_write_results.py
@@ -3,6 +3,7 @@ import sys, os, json, argparse
 import cv2
 import logging as log
 import numpy as np
+import pandas as pd
 from openvino.inference_engine import IECore
 
 
@@ -51,8 +52,11 @@ if __name__ == "__main__":
 
     n, c, h, w = net.inputs[input_blob].shape
     images = np.ndarray(shape=(n, c, h, w))
+    images_hw = []
     for i in range(n):
         image = cv2.imread(args.input[i])
+        image_height, image_width = image.shape[:-1]
+        images_hw.append((image_height), (image_width))
         if image.shape[:1] != (h, w):
             log.warning(
                 "Image {} is resized from {} to {}".format(
@@ -70,6 +74,57 @@ if __name__ == "__main__":
     res = exec_net.infer(inputs={input_blob: images})
     # print(res)
     print("\nType of result object", type(res))
+
+    res = res[out_blob]
+    data = res[0][0]
+    combined_data = []
+    for number, proposal in enumerate(data):
+        if proposal[2] > 0:
+            image_id = np.int(proposal[0])
+            image_height, image_width = images_hw[image_id]
+            label = np.int(proposal[1])
+            confidence = proposal[2]
+            xmin = np.int(image_width * proposal[3])
+            ymin = np.int(image_height * proposal[4])
+            xmax = np.int(image_width * proposal[5])
+            ymax = np.int(image_height * proposal[6])
+            if proposal[2] > 0.5:
+                combination_str = (
+                    str(image_id)
+                    + " "
+                    + str(image_width)
+                    + " "
+                    + str(image_height)
+                    + " "
+                    + str(label)
+                    + " "
+                    + str(xmin)
+                    + " "
+                    + str(ymin)
+                    + " "
+                    + str(xmax)
+                    + " "
+                    + str(ymax)
+                    + " "
+                    + str(confidence)
+                )
+                combined_data.append([combination_str.strip()])
+
+    dataframe = pd.DataFrame(
+        combined_data,
+        columns=[
+            "filename",
+            "width",
+            "height",
+            "class",
+            "xmin",
+            "ymin",
+            "xmax",
+            "ymax",
+            "score",
+        ],
+    )
+    dataframe.to_csv("output" + ".csv", index=False)
 
     res_new = {}
     for key, val in res.items():
